eaa14b11d3ceb911b661565f092b232a3c87ef18
Fernando Castro <facastro@sudamericano.edu.ec>
2025-04-24 10:18:34 -0500

    primero

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [CommonModule, IonicModule, FormsModule],
+  providers: [Storage],
+  template: `
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>Diario de Aventuras Fernando</ion-title>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-content class="ion-padding">
+    <ion-item>
+      <ion-input placeholder="Título" [(ngModel)]="title"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-textarea placeholder="Descripción" [(ngModel)]="description"></ion-textarea>
+    </ion-item>
+    <ion-button expand="full" (click)="saveAdventure()">Guardar</ion-button>
+
+    <ion-list>
+      <ion-item *ngFor="let adventure of adventures">
+        <ion-label>
+          <h2>{{ adventure.title }}</h2>
+          <p>{{ adventure.description }}</p>
+        </ion-label>
+      </ion-item>
+    </ion-list>
+  </ion-content>
+  `
 })
 export class HomePage {
-  constructor() {}
+  title = '';
+  description = '';
+  adventures: any[] = [];
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    await this.storage.create();
+    this.loadAdventures();
+  }
+
+  async saveAdventure() {
+    if (!this.title || !this.description) return;
+
+    const newAdventure = {
+      title: this.title,
+      description: this.description
+    };
+
+    const stored = await this.storage.get('adventures') || [];
+    stored.push(newAdventure);
+    await this.storage.set('adventures', stored);
+
+    this.title = '';
+    this.description = '';
+    this.loadAdventures();
+  }
+
+  async loadAdventures() {
+    this.adventures = await this.storage.get('adventures') || [];
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+import { provideStorage } from '@ionic/storage-angular';
+import { PLATFORM_ID } from '@angular/core';
+
+function storageProviderFactory(platformId: Object) {
+  return provideStorage(platformId, {
+    name: '__mydb',
+    driverOrder: ['indexeddb', 'localstorage']
+  });
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    {
+      provide: 'STORAGE_PROVIDER',
+      useFactory: storageProviderFactory,
+      deps: [PLATFORM_ID]
+    }
   ],
 });
